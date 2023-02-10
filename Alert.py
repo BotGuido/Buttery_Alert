7f4350b36fb1827687127347d699a7a2a7ae394d
Afanasev Sergei <afanasevsergeia@gmail.com>
2023-02-11 01:54:39 +0300

    Total

---
+++ Alert.py
@@ -0,0 +1,19 @@
+# pip install psutil 
+# pip install py-notifier
+# pip install win10toast
+
+
+import psutil
+from pynotifier import Notification
+
+battery = psutil.sensors_battery()
+plugged = battery.power_plugged
+percent = battery.percent
+
+if percent <= 30 and plugged != True:
+    Notification(
+        title="Battery Low",
+        description=str(percent) + "% Battery remain!!",
+        duration=5,  # Duration in seconds
+
+    ).send()
